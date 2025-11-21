3806e97be07ec72a93fe2c478631b6b83499bfac
Vipparla Veeranjaneyulu <145988161+veera5423@users.noreply.github.com>
2025-11-21 10:07:35 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,17 +10,17 @@ from routes.career_explorer import career_explorer_bp
 from routes.public_resources import public_resources_bp
 from routes.admin import admin_bp
 from flask_mail import Mail
-from werkzeug.security import generate_password_hash, check_password_hash
 
 mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app, origins=["http://localhost:5173",Config.FRONTEND], supports_credentials=True, allow_headers=["*"])
+
     jwt.init_app(app)
     mail.init_app(app)
 
+    # Register blueprints
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(ai_summarizer_bp, url_prefix="/api/summarizer")
     app.register_blueprint(ai_quiz_bp, url_prefix="/api/quiz")
@@ -28,16 +28,21 @@ def create_app():
     app.register_blueprint(career_explorer_bp, url_prefix='/api/career')
     app.register_blueprint(public_resources_bp, url_prefix='/api/public')
     app.register_blueprint(admin_bp, url_prefix='/api/admin')
-    # ps=generate_password_hash("adminpassword")
 
-    # print(f"Admin password hash: {ps}")
     
+    CORS(
+        app,
+        origins=["http://localhost:5173", Config.FRONTEND],
+        supports_credentials=True,
+        allow_headers="*",
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    )
+
     @app.route("/")
     def home():
         return {"message": "Flask API Running 🚀"}
 
     return app
-
 
 if __name__ == "__main__":
     app = create_app()
